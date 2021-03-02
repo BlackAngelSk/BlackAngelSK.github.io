137639f6f37ec69029a6042e1ff155e75733dbc7
BlackAngelSk <51242429+BlackAngelSk@users.noreply.github.com>
2021-03-02 22:08:01 +0100

    done 4 now

--- cheat/style/js/title.js
+++ cheat/style/js/title.js
@@ -3,7 +3,7 @@ const chars = ['$','%','#','@','&','(',')','=','*','/'];
 const charsTotal = chars.length;
 const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-let title = 'Blackangelsk.github.io';
+let title = 'configs/luas';
 let i = 0;
 setInterval(() => {
 	
